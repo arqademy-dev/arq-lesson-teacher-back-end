70511266a10407802233a7925ee9b9d9330c8acd
Koji Joshua <jjameskoji@gmail.com>
2026-07-24 10:12:13 +0100

    New commit 002

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,6 @@ const isVercel = !!process.env.VERCEL;
 if (!isVercel) {
   const port = Number(process.env.PORT) || 3000;
   app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
   });
 }
